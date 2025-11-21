c0d9c0a9adda9d92b57c65b0af0fa5d92fd3147b
Yuthika <yuthikavj@gmail.com>
2025-11-21 16:39:26 +0530

    Initialize

---
+++ directory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Create a directory named "my_directory"
+mkdir my_directory
+
+#change Directory
+cd my_directory
+
+#Create an empty file named "my_file.txt" inside "my_directory"
+touch my_file.txt
+
+#Write text in my_file.txt
+echo "This is a sample file." > my_file.txt
